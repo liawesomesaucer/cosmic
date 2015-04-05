591e75e7f1786a2d8df4aec2f5de2de729cbb893
Vincent <vincent.oddrepublic@gmail.com>
2015-04-05 12:54:17 -0700

    fdsafs

--- db.py
+++ db.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
-
+HEROKU_POSTGRESQL_CRIMSON_URL: "postgres://svzgijjedzoxgo:lqiOequvQPlltINbMVmdLKKf1e@ec2-54-163-235-165.compute-1.amazonaws.com:5432/dct7rhlbci5ha1"
 DB_URL = "postgres://nxuzisdyscgaic:XcSLKwBZ684bt6EMSml59pPcgm@ec2-54-163-235-165.compute-1.amazonaws.com:5432/d60q0hmibvq90e"
 
-engine = create_engine(DB_URL, echo=True)
+engine = create_engine(HEROKU_POSTGRESQL_CRIMSON_URL, echo=True)
 
 Session = sessionmaker(bind=engine)
 session = Session()
